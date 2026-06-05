e86016845a07f9bb292ab48d245a14ce179d7813
Miastore <yohannyrico@gmail.com>
2026-06-04 21:13:21 -0500

    inventario

--- app/models/inventario.py
+++ app/models/inventario.py
@@ -1,6 +1,6 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Decimal, DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey, Numeric, DateTime
 from sqlalchemy.sql import func
-from app.config.database import Base # Ajustado a tu carpeta config
+from app.config.database import Base
 
 class InventarioProducto(Base):
     __tablename__ = "inventario_productos"
@@ -12,7 +12,7 @@ class InventarioProducto(Base):
     stock_disponible = Column(Integer, default=0)
     stock_comprometido = Column(Integer, default=0)
     ubicacion_bodega = Column(String(50), nullable=True)
-    peso_kg = Column(Decimal(5, 2), default=0.0)
+    peso_kg = Column(Numeric(5, 2), default=0.0)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
 
@@ -21,7 +21,7 @@ class HistorialInventario(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     producto_id = Column(Integer, ForeignKey("inventario_productos.id", ondelete="CASCADE"), nullable=False)
-    tipo_movimiento = Column(String(20), nullable=False) # 'ENTRADA', 'SALIDA', 'AJUSTE'
+    tipo_movimiento = Column(String(20), nullable=False)
     cantidad = Column(Integer, nullable=False)
     motivo = Column(String(255), nullable=True)
     usuario_id = Column(Integer, nullable=False)
